985b1d537bf474c7fa7485b48e243f9f45409a1d
MeshachOpoku <104779012+MeshachOpoku@users.noreply.github.com>
2024-02-02 20:27:59 -0800

    Add files via upload

---
+++ New Africa.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+
+void them()
+
+{ cout<<"There is a new Africa" ;}
+
+int main()
+{  
+them();
+ return 0;}//knobby be\vvbjhggjgbgc vvbjhggjgbgc vvbjhggjgbgc vvbjhggjgbgc\vvbjhggjgbgc go
